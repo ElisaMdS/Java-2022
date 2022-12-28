44aba031eab443c0e3d701a448f5f000acace446
Elisa Martins dos Santos <elisa99ms@alumni.usp.br>
2022-12-28 12:19:41 +0000

    Conclusão do Módulo 7 - Orientação a Objetos

---
+++ Section 7 - Orientação a Objetos/abstrato/Mamifero.java
@@ -0,0 +1,12 @@
+package abstrato;
+
+public abstract class Mamifero extends Animal {
+
+    public abstract String mamar();
+
+    @Override
+    public String mover() {
+        return "Saindo do lugar";
+
+    }
+}
